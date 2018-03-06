55faaa7c59ea24e843eb7ba2920ef617530011c9
root <root@ip-172-30-12-224.ec2.internal>
2018-03-06 00:29:11 +0000

    adjust message

--- middleware/apikey.js
+++ middleware/apikey.js
@@ -2,7 +2,7 @@
 function middleware(req, res, next){
     var apikey = process.env.PELIAS_API_KEY;
     if (req.query.api_key !== apikey) {
-	return res.status(401).json('I need an API key. Make sure one is configured in your API in PELIAS_API_KEY env var.');
+	return res.status(401).json('I need an API key in GET param api_key.');
     } else {
 	next();
     }
